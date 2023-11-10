42a344a7579dda4d67ea4f03ddf28355af13d166
Michael Lei <michaelleie0ca@gmail.com>
2023-11-09 20:38:25 -0800

    Edit font for pictok

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -2,6 +2,13 @@ import { ReactNode } from 'react'
 import type { Authors } from 'contentlayer/generated'
 import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
+import { Jost } from 'next/font/google'
+
+const jost = Jost({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-jost',
+})
 
 interface Props {
   children: ReactNode
@@ -24,7 +31,7 @@ export default function AuthorLayout({ children, content }: Props) {
             {avatar && (
               <Image src={avatar} alt="avatar" width={198} height={192} className="h-30 w-20" />
             )}
-            <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
+            <h3 className={jost.className + ' pb-2 pt-4 text-2xl font-bold leading-8'}>{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 pt-6">
